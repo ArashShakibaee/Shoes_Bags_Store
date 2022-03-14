25619512fbc6cdc40bb5c17cee55b4bd1900af73
Shakiba <arashshakibaee1001@gmail.com>
2022-03-14 18:39:18 +0330

    create project

---
+++ lib/Modules/Home/Pages/bags_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BagsScreen extends StatefulWidget {
+  static const String routeName = 'bags-screen';
+  const BagsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BagsScreen> createState() => _BagsScreenState();
+}
+
+class _BagsScreenState extends State<BagsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
