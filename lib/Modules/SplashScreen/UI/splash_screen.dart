de40e8f4481cb33cfa899c7ed23884c59eef0415
Shakiba <arashshakibaee1001@gmail.com>
2022-03-14 18:40:32 +0330

    create splash screen

---
+++ lib/Modules/SplashScreen/UI/splash_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:flutter_gen/gen_l10n/app_localization.dart';
+import 'package:shoes_store/Values/app_styles.dart';
+
+class SplashScreen extends StatefulWidget {
+  static const String routeName = 'splash-screen';
+
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
+   late AnimationController _controller;
+   late Animation animation;
+  @override
+  void initState() {
+    _controller = AnimationController(duration: const Duration(seconds: 1),vsync: this);
+    animation = ColorTween(begin: Colors.blueGrey , end: Colors.white).animate(_controller);
+
+    _controller.forward();
+    _controller.addListener(() {setState(() {});});
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Center(
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: LoadingAnimationWidget.hexagonDots(
+                  color: Colors.teal, size: 40),
+            ),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 70),
+                  child: Text(
+                    AppLocalizations.of(context)!.welcomeText,
+                    style: AppStyle.welcomeTextStyle,
+                  ),
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
